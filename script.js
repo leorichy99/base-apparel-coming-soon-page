9d866a9d59f1de50afbe893b17eafc310a039cde
adr99 <leorichy02@gmail.com>
2022-12-08 03:25:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const email = document.querySelector(".email");
 const input = document.querySelector('.input');
 const errorMsg = document.querySelector(".error_message");
 const errorIcon = document.querySelector(".error-icon");
-const errors = ["/", "$", "#"];
+const errors = ["#", "$", "1"];
 
 // show error message when user's email has any of the elements of the errors array in it
 email.addEventListener("input", (e) => {
